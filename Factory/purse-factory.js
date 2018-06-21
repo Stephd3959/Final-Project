78b64b44bbd56fb6ade66fd256421d58d99e1dd7
keyon <keyongrayer@gmail.com>
2018-06-20 21:17:35 -0400

    Functional Project Yay

---
+++ Factory/purse-factory.js
@@ -0,0 +1,70 @@
+(function() {
+  angular.module('app')
+    .factory('PurseFactory', function(DataFactory, $http) {
+      return {
+        combinedApiCall: combinedApiCall
+      }
+
+
+      function geturls () {
+        var answers = DataFactory.getData();
+        var vintage = answers.filter(function(item){
+          return item === 'vintage';
+        });
+        var modern = answers.filter(function(item) {
+          return item === 'modern';
+        });
+        var sporty = answers.filter(function(item) {
+          return item === 'sporty';
+        });
+        var outThere = answers.filter(function(item) {
+          return item === 'out-there';
+        });
+
+        var sortedAnswers = [vintage, modern, sporty, outThere].sort(function(a, b){
+          if(b.length > a.length) return 1;
+          if(a.length > b.length) return -1;
+          return 0;
+        });
+
+        var listingIds = {
+          vintage: ["602850085", "619081339", "569511192", "589777780"],
+          modern: ["473901377"],
+          sporty:  ["620498473", "603089522", "595622097", "568294250"],
+          'out-there': ["620098389"]
+        }
+
+
+        return listingIds[sortedAnswers[0][0]]
+      }
+
+      function combinedApiCall(){
+        var listings = geturls();
+        return Promise.all(listings.map(function(id){
+          return getListing(id).then(function(url){
+            return getImage(id).then(function(src){
+              return { url: url, src: src};
+            });
+          });
+        }));
+      }
+
+
+        function getListing(id) {
+          var url = `https://openapi.etsy.com/v2/listings/${id}/?api_key=jowounhaqslc53jokskfak7v`;
+          return $http.get(url).then(function(response){
+            return response.data.results[0].url;
+          });
+        }
+
+        function getImage(id) {
+          var url = `https://openapi.etsy.com/v2/listings/${id}/images/?api_key=jowounhaqslc53jokskfak7v`;
+          return $http.get(url).then(function(response){
+            return response.data.results[0].url_fullxfull;
+          });
+        }
+
+
+
+    });
+})();
--- purse-results-component/purse-results.controller.js
+++ purse-results-component/purse-results.controller.js
@@ -1,47 +1,13 @@
 (function() {
   angular.module('app')
-    .controller('PurseResultsCtrl', function($http, DataFactory) {
+    .controller('PurseResultsCtrl', function(PurseFactory, $timeout) {
       var $ctrl = this;
-      var a = DataFactory.getData().filter(function(item) {
-        return item.startsWith('vintage');
-      });
-      var b = DataFactory.getData().filter(function(item) {
-        return item.startsWith('modern');
-      });
-      var c = DataFactory.getData().filter(function(item) {
-        return item.startsWith('sporty');
-      });
-      var d = DataFactory.getData().filter(function(item) {
-        return item.startsWith('out-there');
-      });
-      var listingId;
-      var imageId;
+      // $ctrl.etsylinks = PurseFactory.geturls();
+      // $ctrl.image = PurseFactory.imageurls[0];
 
-      if (a.length >= 3) {
-        listingId=["605019944","473901377"];
-      } else if (b.length >=3) {
-        listingId="473901377";
-      } else if (c.length >=3) {
-        listingId="509856400";
-      } else if (d.length >=3) {
-        listingId="620098389";
-      }
-      else {
-        // show the basic purses;
-      }
-
-      for (i=0; i<listingId.length; i++){
-
-        var urlGet = `https://openapi.etsy.com/v2/listings/${listingId[i]}/?api_key=jowounhaqslc53jokskfak7v`;
-        $http.get(urlGet).then(function(response) {
-          $ctrl.testurl = response.data.results[0].url;
-        });
-        var urlImage = `https://openapi.etsy.com/v2/listings/${listingId[i]}/images/1522050662/?api_key=jowounhaqslc53jokskfak7v`;
-        $http.get(urlImage).then(function(response) {
-          $ctrl.test2url = response.data.results[0].url_170x135;
-        });
-
-      };
+      PurseFactory.combinedApiCall().then(function(result){
+        $timeout($ctrl.etsyresults = result);
+      })
 
     });
 })();
